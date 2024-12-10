a34f4b0d8f0da6814418ca60d41fc469bdcf11b1
Rostyslav Liutov <152822419+sleepydraegon108@users.noreply.github.com>
2024-12-10 17:26:03 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,40 +16,29 @@ provider "aws" {
   }
 
   error_document {
-    key = "error.html"  # Optional: specify an error document
+    key = "error.html"  
   }
 }
 
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.static_website.bucket
   key    = "index.html"
-  source = "index.html"  # Path to your index.html file
+  source = "index.html"  
 }
 
 resource "aws_s3_object" "error" {
   bucket = aws_s3_bucket.static_website.bucket
-  key    = "error.html"  # Optional: specify an error document
-  source = "path/to/your/error.html"  # Change to the path of your error.html file if you have one
+  key    = "error.html"  
+  source = "path/to/your/error.html"  
 }
 
 resource "aws_cloudfront_origin_access_identity" "cdn" {
   comment = "Origin Access Identity for S3 bucket"
 }
 
-resource "aws_cloudfront_distribution" "cdn" {
-  origin {
-    domain_name = aws_s3_bucket.static_website.bucket_regional_domain_name
-    origin_id   = "S3Origin"
-
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.cdn.iam_arn
-    }
-  }
-
   enabled             = true
   default_root_object = "index.html"
 
-  # Cache behavior
   default_cache_behavior {
     target_origin_id = "S3Origin"
 
@@ -64,22 +53,19 @@ resource "aws_cloudfront_distribution" "cdn" {
     max_ttl     = 31536000
   }
 
-  # Viewer settings
   viewer_certificate {
     cloudfront_default_certificate = true
   }
 
-  # Price class
   price_class = "PriceClass_All"
 
-  # Restrictions block (optional, but required if you want to restrict access)
+
   restrictions {
     geo_restriction {
       restriction_type = "none"  # Change this if you want to restrict access by geography
     }
   }
 
-  # Optional: Set the comment
   comment = "CloudFront distribution for my-unique-bucket-name-12345 bucket"
 }
 
