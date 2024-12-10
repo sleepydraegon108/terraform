83ebaa33b80aa278a011d4529828f2aaebc6794b
Rostyslav Liutov <152822419+sleepydraegon108@users.noreply.github.com>
2024-12-10 17:05:00 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-2"  # Set to us-east-2
 }
